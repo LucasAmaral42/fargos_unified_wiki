6ddfab8313a55a66c75d8a28774e28134632b78e
LucasAmaral42 <lu.vinicius01@gmail.com>
2022-03-14 23:44:15 -0300

    update endpoints

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,20 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: %i[show edit update destroy]
-
-  # GET /items or /items.json
   def index
-    @items = Item.all
+    render json: Item.all
   end
 
-  # GET /items/1 or /items/1.json
-  def show; end
+  def show
+    @items = Item.in(
+      types: item_params[:types],
+      mod: item_params[:mod]
+    )
+
+    render json: @items.to_json
+  end
 
   private
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
   def item_params
-    params.require(:item).permit(:name, :types, :description, :url, :image_url)
+    params.require(:item).permit(types: [], mod: [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :items
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'items#index'
+  get :items, to: 'items#show'
 end
